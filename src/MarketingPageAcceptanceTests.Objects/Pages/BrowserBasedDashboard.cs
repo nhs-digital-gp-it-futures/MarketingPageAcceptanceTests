447b3e77c23023d9770c6950af4bf41567af9795
Tony Lawson <tony.lawson@bjss.com>
2019-11-28 12:04:57 +0000

    Removed commented out code

--- src/MarketingPageAcceptanceTests.Objects/Pages/BrowserBasedDashboard.cs
+++ src/MarketingPageAcceptanceTests.Objects/Pages/BrowserBasedDashboard.cs
@@ -5,7 +5,6 @@ namespace MarketingPageAcceptanceTests.Objects.Pages
 {
     public class BrowserBasedDashboard : Dashboard
     {
-        //public By Sections => By.ClassName("bc-c-section-list__item");
         public new By SectionTitle => CustomBy.DataTestId("dashboard-section-title", "a");
 
         public By StatusIndicator => CustomBy.DataTestId("dashboard-section-status");
