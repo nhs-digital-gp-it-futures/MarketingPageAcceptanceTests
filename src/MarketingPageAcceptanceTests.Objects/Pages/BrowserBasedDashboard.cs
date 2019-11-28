3b4a4598bf28b935fb2b4f64f71b4bc7cf81f296
Tony Lawson <tony.lawson@bjss.com>
2019-11-28 12:03:43 +0000

    Database and ui had been changed, updated tests

--- src/MarketingPageAcceptanceTests.Objects/Pages/BrowserBasedDashboard.cs
+++ src/MarketingPageAcceptanceTests.Objects/Pages/BrowserBasedDashboard.cs
@@ -3,10 +3,10 @@ using OpenQA.Selenium;
 
 namespace MarketingPageAcceptanceTests.Objects.Pages
 {
-    public class BrowserBasedDashboard
+    public class BrowserBasedDashboard : Dashboard
     {
-        public By Sections => By.ClassName("app-section-list__item");
-        public By SectionTitle => CustomBy.DataTestId("dashboard-section-title", "a");
+        //public By Sections => By.ClassName("bc-c-section-list__item");
+        public new By SectionTitle => CustomBy.DataTestId("dashboard-section-title", "a");
 
         public By StatusIndicator => CustomBy.DataTestId("dashboard-section-status");
     }
