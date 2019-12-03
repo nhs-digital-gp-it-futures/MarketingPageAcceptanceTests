0cf6371c67b568cd66ee407758e8de827e287ae4
Jonathan Alsop <Jonathan.Alsop@bjss.com>
2019-12-03 11:51:10 +0000

    Add contact detail generator

---
+++ src/MarketingPageAcceptanceTests.TestData/ContactDetails/ContactDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketingPageAcceptanceTests.TestData.ContactDetails
+{
+    public sealed class ContactDetail : IContactDetail
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+        public string JobSector { get; set; }
+    }
+}
