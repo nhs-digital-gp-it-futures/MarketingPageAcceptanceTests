ddc0637d8d4be05122484aeaf7ec76d629113437
Tony Lawson <55191681+talawson05@users.noreply.github.com>
2019-12-04 16:14:33 +0000

    added AC for last updated date (#31)
    * added AC for last updated date
    
    * Amended assertion criteria based on clarifications
    
    * Parameterised assertion step

---
+++ src/MarketingPageAcceptanceTestsSpecflow/Steps/AboutSolution/LastUpdatedDate.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using MarketingPageAcceptanceTestsSpecflow.Utils;
+using TechTalk.SpecFlow;
+
+namespace MarketingPageAcceptanceTestsSpecflow.Steps.AboutSolution
+{
+    [Binding]
+    public class LastUpdatedDate
+    {
+        private UITest _test;
+        private ScenarioContext _context;
+        public LastUpdatedDate(UITest test, ScenarioContext context)
+        {
+            _test = test;
+            _context = context;
+        }
+
+        [Given(@"that the Solution Summary is updated")]
+        public void GivenThatTheSolutionSummaryIsUpdated()
+        {
+            _context.Pending();
+        }
+        
+        [Given(@"that the About Solution URL is updated")]
+        public void GivenThatTheAboutSolutionURLIsUpdated()
+        {
+            _context.Pending();
+        }
+        
+        [Given(@"that the Contact details are updated")]
+        public void GivenThatTheContactDetailsAreUpdated()
+        {
+            _context.Pending();
+        }
+        
+        [When(@"the content has been updated")]
+        public void WhenTheContentHasBeenUpdated()
+        {
+            _context.Pending();
+        }
+
+        [Then(@"the Last Changed Date is updated in the (.*) table")]
+        public void ThenTheLastChangedDateIsUpdated(string tableName)
+        {
+            _context.Pending();
+        }
+
+    }
+}
