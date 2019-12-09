1744bf4d18debbf4ff5ea5ce372e9e35f968ebdd
Tony Lawson <tony.lawson@bjss.com>
2019-12-09 11:57:00 +0000

    3602

---
+++ src/MarketingPageAcceptanceTestsSpecflow/Steps/ClientApplication/MobileFirst/MobileFirst.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MarketingPageAcceptanceTestsSpecflow.Utils;
+using TechTalk.SpecFlow;
+
+namespace MarketingPageAcceptanceTestsSpecflow.Steps.ClientApplication.MobileFirst
+{
+    [Binding]
+    public class MobileFirst : TestBase
+    {
+
+        public MobileFirst(UITest test, ScenarioContext context) : base(test, context)
+        {
+                
+        }
+
+        [Given(@"that an answer is provided to the mobile first question")]
+        public void GivenThatAnAnswerIsProvidedToTheMobileFirstQuestion()
+        {
+            _context.Pending();
+        }
+    }
+}
