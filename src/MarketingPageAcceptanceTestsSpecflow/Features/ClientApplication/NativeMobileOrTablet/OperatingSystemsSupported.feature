a1c8f72175b6804329dee1831f4f39c6bf5a72f5
Jonathan Alsop <Jonathan.Alsop@bjss.com>
2019-12-20 14:05:28 +0000

    Add Supported Operating system section and steps

---
+++ src/MarketingPageAcceptanceTestsSpecflow/Features/ClientApplication/NativeMobileOrTablet/OperatingSystemsSupported.feature
@@ -0,0 +1,43 @@
+﻿Feature: OperatingSystemsSupported
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+Scenario: Mandatory - Yes
+Given that a User has provided a value for the Mandatory Information
+When the User is managing their Operating System Information
+Then the Supported operating systems Sub-Section is marked as Complete 
+
+Scenario: Data not Saved
+Given that an answer has not been provided to the mandatory question
+When the User exits the page
+Then the Supported operating systems Sub-Section is marked as Incomplete 
+
+Scenario: Supported operating systems does not exceed maximum
+Given the User has entered text
+And it does not exceed the maximum character count
+When the User attempts to save 
+Then the Section is saved
+
+Scenario: Supported operating systems does exceed maximum
+Given the User has entered text
+And it does exceed the maximum character count
+When the User attempts to save 
+Then the Section is not saved 
+And an indication is given to the User as to why
+
+Scenario: Appear on Preview - Supported operating systems
+Given that data has been saved in this section
+When a User previews the Marketing Page
+Then data will be presented on the Preview of the Marketing Page
+
+Scenario: Supported operating systems Mandatory Data Missing Validation
+Given that a User has not provided any mandatory data
+When the User attempts to save
+Then the Section is not saved 
+And an indication is given to the User as to why
+
+Scenario: Supported operating systems Validation Error Message Anchors
+Given validation has been triggered
+When the User selects an error link in the Error Summary
+Then the User will be navigated to the relevant section on the page
---
+++ src/MarketingPageAcceptanceTestsSpecflow/Steps/ClientApplication/NativeMobileOrTablet/OperatingSystemsSupported.cs
@@ -0,0 +1,55 @@
+﻿using MarketingPageAcceptanceTestsSpecflow.Utils;
+using TechTalk.SpecFlow;
+
+namespace MarketingPageAcceptanceTestsSpecflow.Steps.ClientApplication.NativeMobileOrTablet
+{
+    [Binding]
+    public class OperatingSystemsSupported : TestBase
+    {
+        public OperatingSystemsSupported(UITest test, ScenarioContext context) : base(test, context)
+        {
+        }
+
+        [Given(@"that a User has provided a value for the Mandatory Information")]
+        public void GivenThatAUserHasProvidedAValueForTheMandatoryInformation()
+        {
+            _context.Pending();
+        }
+
+        [Given(@"that an answer has not been provided to the mandatory question")]
+        public void GivenThatAnAnswerHasNotBeenProvidedToTheMandatoryQuestion()
+        {
+            _context.Pending();
+        }
+
+        [Given(@"the User has entered text")]
+        public void GivenTheUserHasEnteredText()
+        {
+            _context.Pending();
+        }
+
+        [Given(@"that a User has not provided any mandatory data")]
+        public void GivenThatAUserHasNotProvidedAnyMandatoryData()
+        {
+            _context.Pending();
+        }
+
+        [When(@"the User is managing their Operating System Information")]
+        public void WhenTheUserIsManagingTheirOperatingSystemInformation()
+        {
+            _context.Pending();
+        }
+
+        [Then(@"the Section is marked as '(.*)' on the Native Desktop Client Type Sub-Form")]
+        public void ThenTheSectionIsMarkedAsOnTheNativeDesktopClientTypeSub_Form(string status)
+        {
+            _context.Pending();
+        }
+
+        [Then(@"the Section is marked as '(.*)' on the Browser Based Client Type Sub-Form")]
+        public void ThenTheSectionIsMarkedAsOnTheBrowserBasedClientTypeSub_Form(string status)
+        {
+            _context.Pending();
+        }
+    }
+}
