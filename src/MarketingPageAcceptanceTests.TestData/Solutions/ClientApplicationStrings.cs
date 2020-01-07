482504d01312c55688465df4ff06843e5968495f
Tony Lawson <tony.lawson@bjss.com>
2020-01-07 13:20:49 +0000

    refactored ClientApplicationStrings to make it more maintainable

--- src/MarketingPageAcceptanceTests.TestData/Solutions/ClientApplicationStrings.cs
+++ src/MarketingPageAcceptanceTests.TestData/Solutions/ClientApplicationStrings.cs
@@ -2,69 +2,112 @@
 {
     public static class ClientApplicationStrings
     {
-        const string clientApplicationCompleted = "{\"ClientApplicationTypes\":[\"browser-based\"],\"BrowsersSupported\":[\"Google Chrome\"],\"MobileResponsive\":true,\"Plugins\":{\"Required\":false,\"AdditionalInformation\":\"\"},\"MinimumConnectionSpeed\":\"1Mbps\",\"MinimumDesktopResolution\":\"21:9-2560 x 1080\",\"HardwareRequirements\":null,\"NativeMobileHardwareRequirements\":null,\"AdditionalInformation\":null,\"MobileFirstDesign\":true,\"NativeMobileFirstDesign\":true,\"MobileOperatingSystems\":{\"OperatingSystems\":[\"Apple IOS\",\"Android\"],\"OperatingSystemsDescription\":\"\"},\"MobileConnectionDetails\":{\"MinimumConnectionSpeed\":\"\",\"ConnectionType\":[],\"Description\":\"\"},\"MobileMemoryAndStorage\":{\"MinimumMemoryRequirement\":\"256MB\",\"Description\":\"Some storage description details\"},\"MobileThirdParty\":{\"ThirdPartyComponents\":\"\",\"DeviceCapabilities\":\"\"},\"NativeMobileAdditionalInformation\":null}";
-        const string clientAppStringTemplate = "{{\"ClientApplicationTypes\":[\"browser-based\"],{0}{1}{2}{3}\"HardwareRequirements\":null,\"NativeMobileHardwareRequirements\":null,\"AdditionalInformation\":null,{4}{5}\"MobileConnectionDetails\":{{\"MinimumConnectionSpeed\":\"\",\"ConnectionType\":[],\"Description\":\"\"}},{6}\"MobileThirdParty\":{{\"ThirdPartyComponents\":\"\",\"DeviceCapabilities\":\"\"}},\"NativeMobileAdditionalInformation\":null}}";
+        const string browserBased = "Browser based";
+        const string nativeMobile = "Native mobile";
+        const string nativeDesktop = "Native desktop";
 
         public static string GetClientAppString(string ignoredSection = null, string clientApplicationTypes = "Browser based")
         {
+            string clientAppString = string.Empty;
             if (string.IsNullOrEmpty(ignoredSection))
             {
-                return parseClientApplicationType(clientApplicationTypes, clientApplicationCompleted);
+                if(clientApplicationTypes.Contains(browserBased))
+                {
+                    clientAppString += getMinimumCompleteBrowserBasedClientApplicationDetails();
+                }
+
+                if (clientApplicationTypes.Contains(nativeMobile))
+                {
+                    clientAppString += getMinimumCompleteNativeMobileClientApplicationDetails();
+                }
+
+                return buildClientApplicationString(clientApplicationTypes, clientAppString);
             }
+
+            if (clientApplicationTypes.Contains(browserBased))
+            {
+                clientAppString += parseBrowserBasedTemplate(ignoredSection);
+            }
+
+            if (clientApplicationTypes.Contains(nativeMobile))
+            {
+                clientAppString += parseNativeMobileTemplate(ignoredSection);
+            }
+
+            return buildClientApplicationString(clientApplicationTypes, clientAppString);
+        }
+
+        private static string buildClientApplicationString(string clientApplicationType, string clientAppString)
+        {
+            clientApplicationType = clientApplicationType.Replace("Browser based", "browser-based");
+            clientApplicationType = clientApplicationType.Replace("Native mobile or tablet", "native-mobile");
+            clientApplicationType = clientApplicationType.Replace("Native desktop", "native-desktop");
+            string completeClientApplicationString = string.Format("{{\"ClientApplicationTypes\":[\"{0}\"],{1}}}", clientApplicationType, clientAppString);
+            return completeClientApplicationString;
+        }
+
+        private static string getMinimumCompleteBrowserBasedClientApplicationDetails()
+        {
+            return "\"BrowsersSupported\":[\"Google Chrome\"],\"MobileResponsive\":true,\"Plugins\":{\"Required\":false,\"AdditionalInformation\":\"\"},\"MinimumConnectionSpeed\":\"1Mbps\",\"MinimumDesktopResolution\":\"21:9-2560 x 1080\",\"HardwareRequirements\":null,\"AdditionalInformation\":null,\"MobileFirstDesign\":true";
+        }
+
+        private static string getMinimumCompleteNativeMobileClientApplicationDetails()
+        {
+            return "\"NativeMobileHardwareRequirements\":null,\"NativeMobileFirstDesign\":true,\"MobileOperatingSystems\":{\"OperatingSystems\":[\"Apple IOS\",\"Android\"],\"OperatingSystemsDescription\":\"\"},\"MobileConnectionDetails\":{\"MinimumConnectionSpeed\":\"\",\"ConnectionType\":[],\"Description\":\"\"},\"MobileMemoryAndStorage\":{\"MinimumMemoryRequirement\":\"256MB\",\"Description\":\"Some storage description details\"},\"MobileThirdParty\":{\"ThirdPartyComponents\":\"\",\"DeviceCapabilities\":\"\"},\"NativeMobileAdditionalInformation\":null";
+        }
+
+        private static string parseBrowserBasedTemplate(string ignoredSection)
+        {
+            string clientAppStringTemplate = "{0}{1}{2}{3}\"HardwareRequirements\":null,\"AdditionalInformation\":null";
+            string formattedString = string.Empty;
 
             var browsersSupported = "\"BrowsersSupported\":[\"Google Chrome\"],\"MobileResponsive\":true,";
             var plugins = "\"Plugins\":{\"Required\":false,\"AdditionalInformation\":\"\"},";
             var connectivityAndResolution = "\"MinimumConnectionSpeed\":\"1Mbps\",\"MinimumDesktopResolution\":\"21:9-2560 x 1080\",";
             var mobileFirstDesign = "\"MobileFirstDesign\":true,";
 
+            switch (ignoredSection)
+            {
+                case "Browsers supported":
+                    formattedString = string.Format(clientAppStringTemplate, "", plugins, connectivityAndResolution, mobileFirstDesign);
+                    break;
+                case "Plug-ins or extensions":
+                    formattedString = string.Format(clientAppStringTemplate, browsersSupported, "", connectivityAndResolution, mobileFirstDesign);
+                    break;
+                case "Connectivity and resolution":
+                    formattedString = string.Format(clientAppStringTemplate, browsersSupported, plugins, "", mobileFirstDesign);
+                    break;
+                case "Mobile first":
+                    formattedString = string.Format(clientAppStringTemplate, browsersSupported, plugins, connectivityAndResolution, "");
+                    break;
+            }
+
+            return formattedString;            
+        }
+
+        private static string parseNativeMobileTemplate(string ignoredSection)
+        {
+            string clientAppStringTemplate = "\"NativeMobileHardwareRequirements\":null,{0}{1}\"MobileConnectionDetails\":{{\"MinimumConnectionSpeed\":\"\",\"ConnectionType\":[],\"Description\":\"\"}},{2}\"MobileThirdParty\":{{\"ThirdPartyComponents\":\"\",\"DeviceCapabilities\":\"\"}},\"NativeMobileAdditionalInformation\":null";
+            string formattedString = string.Empty;
+
             var mobileOperatingSystemsSupported = "\"MobileOperatingSystems\":{\"OperatingSystems\":[\"Apple IOS\",\"Android\"],\"OperatingSystemsDescription\":\"\"},";
             var nativeMobileFirstDesign = "\"NativeMobileFirstDesign\":true,";
             var mobileMemoryAndStorage = "\"MobileMemoryAndStorage\":{\"MinimumMemoryRequirement\":\"256MB\",\"Description\":\"Some storage description details\"},";
 
-            string clientAppString = string.Empty;
-
-            //to differentiate between browser and native mobile 'mobile first' sections
-            if (ignoredSection.Equals("Mobile first") && clientApplicationTypes.Contains("Native mobile"))
-            {
-                ignoredSection = "Native mobile first";
-            }
-
             switch (ignoredSection)
             {
-                case "Browsers supported":
-                    clientAppString = string.Format(clientAppStringTemplate, "", plugins, connectivityAndResolution, mobileFirstDesign, nativeMobileFirstDesign, mobileOperatingSystemsSupported, mobileMemoryAndStorage);
-                    break;
-                case "Plug-ins or extensions":
-                    clientAppString = string.Format(clientAppStringTemplate, browsersSupported, "", connectivityAndResolution, mobileFirstDesign, nativeMobileFirstDesign, mobileOperatingSystemsSupported, mobileMemoryAndStorage);
-                    break;
-                case "Connectivity and resolution":
-                    clientAppString = string.Format(clientAppStringTemplate, browsersSupported, plugins, "", mobileFirstDesign, nativeMobileFirstDesign, mobileOperatingSystemsSupported, mobileMemoryAndStorage);
-                    break;
                 case "Mobile first":
-                    clientAppString = string.Format(clientAppStringTemplate, browsersSupported, plugins, connectivityAndResolution, "", nativeMobileFirstDesign, mobileOperatingSystemsSupported, mobileMemoryAndStorage);
-                    break;
-                case "Native mobile first":
-                    clientAppString = string.Format(clientAppStringTemplate, browsersSupported, plugins, connectivityAndResolution, mobileFirstDesign, "", mobileOperatingSystemsSupported, mobileMemoryAndStorage);
+                    formattedString = string.Format(clientAppStringTemplate, "", mobileOperatingSystemsSupported, mobileMemoryAndStorage);
                     break;
                 case "Supported operating systems":
-                    clientAppString = string.Format(clientAppStringTemplate, browsersSupported, plugins, connectivityAndResolution, mobileFirstDesign, nativeMobileFirstDesign, "", mobileMemoryAndStorage);
+                    formattedString = string.Format(clientAppStringTemplate, nativeMobileFirstDesign, "", mobileMemoryAndStorage);
                     break;
                 case "Memory and storage":
-                    clientAppString = string.Format(clientAppStringTemplate, browsersSupported, plugins, connectivityAndResolution, mobileFirstDesign, nativeMobileFirstDesign, mobileOperatingSystemsSupported, "");
+                    formattedString = string.Format(clientAppStringTemplate, nativeMobileFirstDesign, mobileOperatingSystemsSupported, "");
                     break;
             }
 
-            return parseClientApplicationType(clientApplicationTypes, clientAppString);
-        }
-
-        private static string parseClientApplicationType(string clientApplicationType, string clientAppString)
-        {
-            clientApplicationType = clientApplicationType.Replace("Browser based", "browser-based");
-            clientApplicationType = clientApplicationType.Replace("Native mobile or tablet", "native-mobile");
-            clientApplicationType = clientApplicationType.Replace("Native desktop", "native-desktop");
-            string newFormattedClientApplicationType = string.Format("\"ClientApplicationTypes\":[\"{0}\"]", clientApplicationType);
-            string formattedClientApplicationString = clientAppString.Replace("\"ClientApplicationTypes\":[\"browser-based\"]", newFormattedClientApplicationType);
-            return formattedClientApplicationString;
+            return formattedString;            
         }
     }
 }
