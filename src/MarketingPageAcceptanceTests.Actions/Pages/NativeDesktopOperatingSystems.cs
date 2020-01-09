c128af73398bc4ae53e5abba355e89b9d782cdd2
Tony Lawson <55191681+talawson05@users.noreply.github.com>
2020-01-09 15:27:38 +0000

    4165 (#62)
    * Implemented scenarios for native desktop supported operating systems
    
    * Instantiating objects

---
+++ src/MarketingPageAcceptanceTests.Actions/Pages/NativeDesktopOperatingSystems.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MarketingPageAcceptanceTests.TestData.Information;
+using OpenQA.Selenium;
+
+namespace MarketingPageAcceptanceTests.Actions.Pages
+{
+    public class NativeDesktopOperatingSystems : PageAction
+    {
+        public NativeDesktopOperatingSystems(IWebDriver driver) : base(driver)
+        {
+
+        }
+
+        public void TextAreaSendText(int characters)
+        {
+            var text = RandomInformation.RandomString(characters);
+
+            driver.FindElement(pages.NativeDesktopSections.SupportedOperatingSystems.OperatingSystemsDescription).SendKeys(text);
+        }
+    }
+}
