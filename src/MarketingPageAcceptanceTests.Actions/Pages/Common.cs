46e845b2b4b296a8a5019f78fd5660f2d66732fd
Jonathan Alsop <Jonathan.Alsop@bjss.com>
2020-01-23 11:29:12 +0000

    Remove references to DotNetSeleniumExtras

--- src/MarketingPageAcceptanceTests.Actions/Pages/Common.cs
+++ src/MarketingPageAcceptanceTests.Actions/Pages/Common.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using MarketingPageAcceptanceTests.Actions.Pages.Utils;
+using MarketingPageAcceptanceTests.Actions.Utils;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
@@ -103,13 +104,13 @@ namespace MarketingPageAcceptanceTests.Actions.Pages
         {
             // Using Submit() directly to the form instead of Click() on the button prevents HTTP timeouts to Selenium server errors in 95% of cases
             wait.Until(s => s.FindElement(By.TagName("form")).Displayed);
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(pages.Common.SectionSaveAndReturn));
+            wait.Until(ElementExtensions.ElementToBeClickable(pages.Common.SectionSaveAndReturn));
             driver.FindElement(By.TagName("form")).Submit();            
         }
 
         public void WaitUntilSectionPageNotShownAnymore()
         {
-            wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.InvisibilityOfElementLocated(By.TagName("form")));
+            wait.Until(ElementExtensions.InvisibilityOfElement(By.TagName("form")));
         }
     }
 }
--- src/MarketingPageAcceptanceTests.Actions/Utils/Extensions.cs
+++ src/MarketingPageAcceptanceTests.Actions/Utils/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace MarketingPageAcceptanceTests.Actions.Utils
 {
@@ -31,6 +32,52 @@ namespace MarketingPageAcceptanceTests.Actions.Utils
         {
             wait.Until(s => s.FindElements(elementBy)[index].Enabled);
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].value=arguments[1];", driver.FindElements(elementBy)[index], value);
+        }
+
+        internal static Func<IWebDriver, bool> InvisibilityOfElement(By locator)
+        {
+            return (driver) =>
+            {
+                try
+                {
+                    var element = driver.FindElement(locator);
+                    return !element.Displayed;
+                }
+                catch (NoSuchElementException)
+                {
+                    // Returns true because the element is not present in DOM. 
+                    //The try block checks if the element is present but is invisible.
+                    return true;
+                }
+                catch (StaleElementReferenceException)
+                {
+                    // Returns true because stale element reference implies that element is no longer visible.
+                    return true;
+                }
+            };
+        }
+
+        internal static Func<IWebDriver, IWebElement> ElementToBeClickable(By locator)
+        {
+            return (driver) =>
+            {
+                var element = driver.FindElement(locator).Displayed ? driver.FindElement(locator) : null;
+                try
+                {
+                    if (element != null && element.Enabled)
+                    {
+                        return element;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                catch (StaleElementReferenceException)
+                {
+                    return null;
+                }
+            };
         }
     }
 }
