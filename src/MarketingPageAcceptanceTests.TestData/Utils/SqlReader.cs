af8ee6d0c088547528734812e5a68a492f6eaac6
Jonathan Alsop <Jonathan.Alsop@bjss.com>
2020-01-30 14:44:26 +0000

    Moving whereretry policy runs

--- src/MarketingPageAcceptanceTests.TestData/Utils/SqlReader.cs
+++ src/MarketingPageAcceptanceTests.TestData/Utils/SqlReader.cs
@@ -9,22 +9,23 @@ namespace MarketingPageAcceptanceTests.TestData.Utils
     {
         internal static T Read<T>(string connectionString, string query, SqlParameter[] sqlParameters, Func<IDataReader, T> mapDataReader)
         {
-            T returnValue = default;
+            T returnValue = default; ;
 
-            Policy.Handle<SqlException>()
-                .Or<TimeoutException>()
-                .WaitAndRetry(3, retryAttempt => TimeSpan.FromMilliseconds(500))
-                .Execute(() =>
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand(query, connection))
                 {
-                    using (SqlConnection connection = new SqlConnection(connectionString))
-                    {
-                        connection.Open();
+                    //add the params
+                    command.Parameters.AddRange(sqlParameters);
 
-                        using (var command = new SqlCommand(query, connection))
+                    Policy.Handle<SqlException>()
+                        .Or<TimeoutException>()
+                        .WaitAndRetry(3, retryAttempt => TimeSpan.FromMilliseconds(500))
+                        .Execute(() =>
                         {
-                            //add the params
-                            command.Parameters.AddRange(sqlParameters);
-
                             SqlDataReader reader = command.ExecuteReader();
                             try
                             {
@@ -35,9 +36,10 @@ namespace MarketingPageAcceptanceTests.TestData.Utils
                                 reader.Close();
                             }
                         }
-                    }
+                    );
                 }
-            );
+            }
+
 
             return returnValue;
         }
