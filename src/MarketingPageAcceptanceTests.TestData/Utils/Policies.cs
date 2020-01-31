458407a09b226e9fdeebbbf30d28b5b8909dd476
Jonathan Alsop <Jonathan.Alsop@bjss.com>
2020-01-31 14:07:52 +0000

    Add retry policies for SqlReader in separate class

---
+++ src/MarketingPageAcceptanceTests.TestData/Utils/Policies.cs
@@ -0,0 +1,18 @@
+﻿using Polly;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace MarketingPageAcceptanceTests.TestData.Utils
+{
+    internal static class Policies
+    {
+        internal static ISyncPolicy RetryPolicy()
+        {
+            return Policy.Handle<SqlException>()
+                .Or<TimeoutException>()
+                .WaitAndRetry(3, retryAttempt => TimeSpan.FromMilliseconds(500));
+        }
+    }
+}
