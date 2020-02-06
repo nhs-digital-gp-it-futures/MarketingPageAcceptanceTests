306721498038085fb6f70d62770be773bc336027
Tony Lawson <tony.lawson@bjss.com>
2020-02-06 10:29:32 +0000

    changed an enter text to help waits/timeouts

--- src/MarketingPageAcceptanceTests.Actions/Pages/EditFeatures.cs
+++ src/MarketingPageAcceptanceTests.Actions/Pages/EditFeatures.cs
@@ -1,6 +1,8 @@
 ﻿using MarketingPageAcceptanceTests.Actions.Pages.Utils;
+using MarketingPageAcceptanceTests.Actions.Utils;
 using MarketingPageAcceptanceTests.TestData.Information;
 using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 
 namespace MarketingPageAcceptanceTests.Actions.Pages
@@ -18,22 +20,10 @@ namespace MarketingPageAcceptanceTests.Actions.Pages
         /// <param name="characterCount">The number of characters to add</param>
         public string AddTextToFeature(int characterCount = 100)
         {
-            var randomTextField = GetRandomTextField();
             string randomString = RandomInformation.RandomString(characterCount);
-            randomTextField.Clear();
-            randomTextField.SendKeys(randomString);
+            var randomIndex = new Random().Next(driver.FindElements(pages.EditFeatures.FeatureText).Count);    
+            driver.EnterTextViaJS(wait, pages.EditFeatures.FeatureText, randomString, randomIndex);
             return randomString;
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns>Returns a random text field</returns>
-        private IWebElement GetRandomTextField()
-        {
-            IList<IWebElement> textFields = driver.FindElements(pages.EditFeatures.FeatureText);
-
-            return RandomInformation.GetRandomItem(textFields);
         }
 
         /// <summary>
