65d35497799352fc65e8a5b84ec8f4d9754094dc
PhysicsJon <j.r.alsop.18@gmail.com>
2020-02-12 15:34:43 +0000

    Adding Epics queries and POCOs and Generator

--- src/MarketingPageAcceptanceTests.TestData/Capabilities/CapabilitiesGenerator.cs
+++ src/MarketingPageAcceptanceTests.TestData/Capabilities/CapabilitiesGenerator.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MarketingPageAcceptanceTests.TestData.Information;
+using MarketingPageAcceptanceTests.TestData.Solutions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -22,5 +24,49 @@ namespace MarketingPageAcceptanceTests.TestData.Capabilities
 
             return solCaps;
         }
+
+        public static IEnumerable<SolutionCapabilityEpics> GenerateCapabilityEpics(string connectionString, IEnumerable<SolutionCapabilities> capabilities, Solution solution)
+        {
+            List<SolutionCapabilityEpics> solEpics = new List<SolutionCapabilityEpics>();
+
+            var capabilityIds = new Capability().GetAll(connectionString) // Get list of Capabilities
+                .OrderBy(s => Guid.NewGuid())
+                .Where(s => capabilities.Select(d => d.CapabilityId).Contains(s.CapabilityRef))
+                .ToList();
+
+            foreach(var capability in capabilityIds)
+            {
+                var epics = new Epic().GetAllByCapabilityId(connectionString, capability.Id);
+
+                foreach(var epic in epics)
+                {
+                    solEpics.Add(
+                        new SolutionCapabilityEpics 
+                        { 
+                            SupplierID = solution.SupplierId, 
+                            CapabilityID = capability.CapabilityRef, 
+                            EpicID = epic.Id, 
+                            Level = LevelConversion[epic.Level], 
+                            SolutionID = solution.Id,
+                            EpicFinalAssessmentResult = SelectRandomAssessmentResult()
+                        });
+                }
+            }
+
+            return solEpics;
+        }
+
+        private static string SelectRandomAssessmentResult()
+        {
+            var assessmentLevels = new string[] { "Passed", "Failed", "Not Evidenced" };
+
+            return RandomInformation.GetRandomItem(assessmentLevels);
+        }
+
+        private static Dictionary<string, string> LevelConversion = new Dictionary<string, string>
+        {
+            { "MUST", "Must" },
+            {"MAY", "May" }
+        };
     }
 }
---
+++ src/MarketingPageAcceptanceTests.TestData/Capabilities/SolutionCapabilityEpics.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MarketingPageAcceptanceTests.TestData.Capabilities
+{
+    public sealed class SolutionCapabilityEpics
+    {
+        [Display(Name = "Supplier ID")]
+        public string SupplierID { get; set; }
+        [Display(Name = "Solution ID")]
+        public string SolutionID { get; set; }
+        [Display(Name = "Additional Service ID")]
+        public string AdditionalServiceID { get; set; } = string.Empty;
+        [Display(Name = "Capability ID")]
+        public string CapabilityID { get; set; }
+        [Display(Name = "Epic ID")]
+        public string EpicID { get; set; }        
+        public string Level { get; set; }
+        [Display(Name = "Epic Final Assessment Result")]
+        public string EpicFinalAssessmentResult { get; set; }
+    }
+}
+
