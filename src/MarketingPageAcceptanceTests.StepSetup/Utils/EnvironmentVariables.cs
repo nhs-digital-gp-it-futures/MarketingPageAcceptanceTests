69bb4d6fa0c847f7da241a188b51f3edcd5f5080
Jonathan Alsop <j.r.alsop.18@gmail.com>
2020-08-14 10:16:05 +0100

    Fixes and package updates

--- src/MarketingPageAcceptanceTests.StepSetup/Utils/EnvironmentVariables.cs
+++ src/MarketingPageAcceptanceTests.StepSetup/Utils/EnvironmentVariables.cs
@@ -39,8 +39,8 @@ namespace MarketingPageAcceptanceTests.StepSetup.Utils
         {
             var serverUrl = Environment.GetEnvironmentVariable("SERVERURL") ?? "127.0.0.1,1450";
             var databaseName = Environment.GetEnvironmentVariable("DATABASENAME") ?? "buyingcatalogue";
-            var dbUser = JsonConfigValues("user", "NHSD");
-            var dbPassword = JsonConfigValues("password", "DisruptTheMarket1!");
+            var dbUser = JsonConfigValues("user", "SA");
+            var dbPassword = JsonConfigValues("password", "8VSKwQ8xgk35qWFm8VSKwQ8xgk35qWFm!");
 
             return (serverUrl, databaseName, dbUser, dbPassword);
         }
