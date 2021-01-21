2db4b3b451a0ded10478a94502f7b61d76ca5b81
PhysicsJon <j.r.alsop.18@gmail.com>
2021-01-21 13:30:26 +0000

    Fix EoF newlines

--- src/MarketingPageAcceptanceTests.Actions/Pages/PreviewPage.cs
+++ src/MarketingPageAcceptanceTests.Actions/Pages/PreviewPage.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using MarketingPageAcceptanceTests.Actions.Pages.Utils;
--- src/MarketingPageAcceptanceTests.Objects/Pages/ClientApplicationTypes.cs
+++ src/MarketingPageAcceptanceTests.Objects/Pages/ClientApplicationTypes.cs
@@ -1,11 +1,10 @@
-using OpenQA.Selenium;
+﻿using OpenQA.Selenium;
 
 namespace MarketingPageAcceptanceTests.Objects.Pages
 {
     public static class ClientApplicationTypes
     {
         public static By Checkboxes => By.ClassName("nhsuk-checkboxes__input");
-
 
         public static By CheckboxGroups => By.ClassName("nhsuk-checkboxes__item");
     }
